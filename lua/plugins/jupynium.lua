17feb4d889660d12c93eb23cd59bb01203e34fa4
Chilly <chillywall@163.com>
2025-02-02 14:18:21 +0800

    add jupynium

--- lua/plugins/jupynium.lua
+++ lua/plugins/jupynium.lua
@@ -1,8 +1,4 @@
 return {
   "kiyoon/jupynium.nvim",
-  build = "conda run --no-capture-output -n jupynium pip install .",
-  enabled = vim.fn.isdirectory(vim.fn.expand("~/miniconda3/envs/jupynium")),
-  opts = {
-    python_host = { "conda", "run", "--no-capture-output", "-n", "jupynium", "python" },
-  },
+  build = "pip3 install .",
 }
