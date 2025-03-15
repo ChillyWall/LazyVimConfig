5a92bb9fe25d33d505b5db77ceebe7566dc77dd0
Chilly <chillywall@163.com>
2025-03-15 19:35:08 +0800

    delete jupynium and add extra configures for clangd

---
+++ lua/plugins/clangd.lua
@@ -0,0 +1,20 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      -- Ensure mason installs the server
+      clangd = {
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--function-arg-placeholders",
+          "--fallback-style=llvm",
+          "--query-driver=/usr/bin/g++",
+        },
+      },
+    },
+  },
+}
