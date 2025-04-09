6ee28ed7fa13f8d57722b77d7bbf5760af1d8858
Chilly <chillywall@163.com>
2025-04-09 09:59:18 +0000

    add some tasks for overseer

---
+++ lua/overseer/template/user/catkin_make.lua
@@ -0,0 +1,24 @@
+return {
+  name = "catkin make",
+  builder = function()
+    local cwd = vim.fn.getcwd()
+    return {
+      cmd = {
+        "catkin_make",
+      },
+      args = {
+        "-DCMAKE_COMPILE_COMMANDS=ON",
+      },
+      cwd = vim.fn.getcwd(),
+      components = {
+        { "on_output_quickfix", set_diagnostics = true },
+        "on_result_diagnostics",
+        "default",
+        {
+          "run_after",
+          task_names = { { "shell", cmd = "ln -b -s build/compile_commands.json ./" } },
+        },
+      },
+    }
+  end,
+}
---
+++ lua/plugins/overseer.lua
@@ -0,0 +1,12 @@
+return {
+  "stevearc/overseer.nvim",
+  opts = {
+    templates = {
+      "builtin",
+      "user.c_build",
+      "user.cpp_build",
+      "user.catkin_make",
+      "user.link_compile_commands",
+    },
+  },
+}
