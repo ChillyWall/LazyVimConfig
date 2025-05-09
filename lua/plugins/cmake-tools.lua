8a432cdd6393af8a82bc3b576c9bd30eddb6049d
Chilly <chillywall@163.com>
2025-05-09 12:43:59 +0800

    move some shortcuts to localleader
    CMake and Obsidian related shortcuts use LocalLeader as the prefix, for
    they are only used under cpp projects/markdown notes.
    Also let the shortcuts loaded only when they are needed.

--- lua/plugins/cmake-tools.lua
+++ lua/plugins/cmake-tools.lua
@@ -1,28 +1,31 @@
-local wk = require("which-key")
-wk.add({
-  { "<leader>m", group = "CMake" },
-  { "<leader>ms", group = "Select" },
-  { "<leader>msb", group = "Build" },
+vim.api.nvim_create_autocmd("DirChanged", {
+  callback = function()
+    local cwd = vim.uv.cwd()
+    if vim.fn.filereadable(cwd .. "/CMakeLists.txt") == 1 then
+      local wk = require("which-key")
+      wk.add({
+        { "<localleader>m", group = "CMake" },
+        { "<localleader>ms", group = "Select" },
+        { "<localleader>msb", group = "Build" },
+        { "<localleader>mg", ":CMakeGenerate<CR>", desc = "generate" },
+        { "<localleader>mb", ":CMakeBuild<CR>", desc = "build" },
+        { "<localleader>md", ":CMakeDebug<CR>", desc = "debug" },
+        { "<localleader>mr", ":CMakeRun<CR>", desc = "run" },
+        { "<localleader>mt", ":CMakeRunTest<CR>", desc = "ctest" },
+        { "<localleader>mc", ":CMakeClean<CR>", desc = "clean" },
+        { "<localleader>mi", ":CMakeInstall<CR>", desc = "install" },
+
+        { "<localleader>msbt", ":CMakeSelectBuildType<CR>", desc = "type" },
+        { "<localleader>msbd", ":CMakeSelectBuildDir<CR>", desc = "dir" },
+        { "<localleader>msbg", ":CMakeSelectBuildTarget<CR>", desc = "target" },
+        { "<localleader>msbp", ":CMakeSelectBuildPreset<CR>", desc = "preset" },
+        { "<localleader>msl", ":CMakeSelectLaunchTarget<CR>", desc = "launch target" },
+        { "<localleader>msk", ":CMakeSelectKit<CR>", desc = "kit" },
+        { "<localleader>msc", ":CMakeSelectConfigurePreset<CR>", desc = "configure preset" },
+        { "<localleader>mst", ":CMakeSettings<CR>", desc = "settings" },
+      })
+    end
+  end,
 })
 
-return {
-  "Civitasv/cmake-tools.nvim",
-  keys = {
-    { "<leader>mg", ":CMakeGenerate<CR>", desc = "generate" },
-    { "<leader>mb", ":CMakeBuild<CR>", desc = "build" },
-    { "<leader>md", ":CMakeDebug<CR>", desc = "debug" },
-    { "<leader>mr", ":CMakeRun<CR>", desc = "run" },
-    { "<leader>mt", ":CMakeRunTest<CR>", desc = "ctest" },
-    { "<leader>mc", ":CMakeClean<CR>", desc = "clean" },
-    { "<leader>mi", ":CMakeInstall<CR>", desc = "install" },
-
-    { "<leader>msbt", ":CMakeSelectBuildType<CR>", desc = "type" },
-    { "<leader>msbd", ":CMakeSelectBuildDir<CR>", desc = "dir" },
-    { "<leader>msbg", ":CMakeSelectBuildTarget<CR>", desc = "target" },
-    { "<leader>msbp", ":CMakeSelectBuildPreset<CR>", desc = "preset" },
-    { "<leader>msl", ":CMakeSelectLaunchTarget<CR>", desc = "launch target" },
-    { "<leader>msk", ":CMakeSelectKit<CR>", desc = "kit" },
-    { "<leader>msc", ":CMakeSelectConfigurePreset<CR>", desc = "configure preset" },
-    { "<leader>mst", ":CMakeSettings<CR>", desc = "settings" },
-  },
-}
+return {}
---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,133 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown" },
+  callback = function()
+    local wk = require("which-key")
+    wk.add({
+      { "<localleader>d", ":ObsidianDailies<CR>", desc = "Obsidian dailies" },
+      { "<localleader>t", ":ObsidianTags<CR>", desc = "Obsidian tags" },
+      { "<localleader>c", ":ObsidianTOC<CR>", desc = "Obsidian TOC" },
+      { "<localleader>o", ":ObsidianOpen", desc = "Obsidian open" },
+      { "<localleader>s", ":ObsidianSearch<CR>", desc = "Obsidian search" },
+      { "<localleader>n", ":ObsidianNew<CR>", desc = "Obsidian new note" },
+      { "<localleader>N", ":ObsidianNewFromTemplate", desc = "Obsidian new note from template" },
+      { "<localleader>r", ":ObsidianRename<CR>", desc = "Obsidian rename" },
+      { "<localleader>w", ":ObsidianWorkspace<CR>", desc = "Obsidian workspace" },
+      { "<localleader>l", ":ObsidianLink", desc = "Obsidian link", mode = { "n", "v" } },
+      { "<localleader>L", ":ObsidianLinks<CR>", desc = "Obsidian links" },
+      { "<localleader>q", ":ObsidianQuickSwitch<CR>", desc = "Obsidian quick switch" },
+    })
+  end,
+})
+
+return {
+  "obsidian-nvim/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+
+    -- see below for full list of optional dependencies 👇
+  },
+
+  opts = {
+    -- A list of workspace names, paths, and configuration overrides.
+    -- If you use the Obsidian app, the 'path' of a workspace should generally be
+    -- your vault root (where the `.obsidian` folder is located).
+    -- When obsidian.nvim is loaded by your plugin manager, it will automatically set
+    -- the workspace to the first workspace in the list whose `path` is a parent of the
+    -- current markdown file being edited.
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/vaults/personal",
+      },
+      {
+        name = "learning",
+        path = "~/vaults/learning",
+        -- Optional, override certain settings.
+        overrides = {
+          notes_subdir = "notes",
+        },
+      },
+    },
+
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "notes/dailies",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-%m-%d",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { "daily-notes" },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = nil,
+    },
+
+    -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+    completion = {
+      -- Enables completion using nvim_cmp
+      nvim_cmp = false,
+      -- Enables completion using blink.cmp
+      blink = true,
+      -- Trigger completion at 2 chars.
+      min_chars = 2,
+    },
+
+    templates = {
+      folder = "templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      -- A map for custom variables, the key should be the variable and the value a function
+      substitutions = {},
+    },
+
+    -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
+    -- way then set 'mappings = {}'.
+    mappings = {
+      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Smart action depending on context: follow link, show notes with tag, or toggle checkbox.
+      ["<C-CR>"] = {
+        action = function()
+          return require("obsidian").util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      },
+    },
+
+    picker = {
+      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', 'mini.pick' or 'snacks.pick'.
+      name = "snacks.pick",
+      -- Optional, configure key mappings for the picker. These are the defaults.
+      -- Not all pickers support all mappings.
+      note_mappings = {
+        -- Create a new note from your query.
+        new = "<C-x>",
+        -- Insert a link to the selected note.
+        insert_link = "<C-l>",
+      },
+      tag_mappings = {
+        -- Add tag(s) to current note.
+        tag_note = "<C-x>",
+        -- Insert a tag at the current location.
+        insert_tag = "<C-l>",
+      },
+    },
+  },
+}
